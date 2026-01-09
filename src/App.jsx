cabf05e44b2bad5e96ae46bed4a56a7ae2ab26c8
deb0pam <debopamdas8@gmail.com>
2026-01-09 18:56:40 +0530

    hero section implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import NavBar from "./components/NavBar.jsx";
+import Hero from "./components/Hero.jsx";
 const App=()=>{
   return(
     <main>
       <NavBar/>
+      <Hero/>
     </main>
   )
 }
