7dca9de3c2b16e639a5143e2daa3436dbfc2d7d9
deb0pam <debopamdas8@gmail.com>
2026-01-09 19:40:13 +0530

    product view done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import NavBar from "./components/NavBar.jsx";
 import Hero from "./components/Hero.jsx";
+import ProductViewer from "./components/ProductViewer.jsx";
 const App=()=>{
   return(
     <main>
       <NavBar/>
       <Hero/>
+      <ProductViewer/>
     </main>
   )
 }
