8ae402acb62910021b1fbb01fd96a5a09e7858ab
deb0pam <debopamdas8@gmail.com>
2026-01-10 17:29:11 +0530

    implement features section

--- src/components/models/Macbook-14.jsx
+++ src/components/models/Macbook-14.jsx
@@ -17,9 +17,7 @@ import {Color,SRGBColorSpace} from "three"
 export default function MacbookModel14(props) {
   const {color}=useMacbookStore();
   const { nodes, materials,scene } = useGLTF('/models/macbook-14-transformed.glb')
-  const texture=useTexture('/screen.png')
-  texture.colorSpace=SRGBColorSpace;
-  texture.needsUpdate=true;
+  const texture=useTexture('/screen.png', (t) => { t.colorSpace = SRGBColorSpace; t.needsUpdate = true; })
   useEffect(()=>{
     scene.traverse((child)=>{
       if(child.isMesh){
