a197a327bf94a8899fcb523c65882bef52d40130
deb0pam <debopamdas8@gmail.com>
2026-01-10 10:53:40 +0530

    imlement 3d product viewer section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import NavBar from "./components/NavBar.jsx";
 import Hero from "./components/Hero.jsx";
 import ProductViewer from "./components/ProductViewer.jsx";
+import {ScrollTrigger} from "gsap/all";
+import gsap from 'gsap';
+gsap.registerPlugin(ScrollTrigger)
 const App=()=>{
   return(
     <main>
